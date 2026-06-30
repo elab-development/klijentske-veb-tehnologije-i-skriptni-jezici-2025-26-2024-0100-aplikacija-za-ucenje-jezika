022c8a947d2607bdd984ff986cd18d8ba75efdd0
mateja1234567 <matejabrkovic121@gmail.com>
2026-06-30 21:33:08 +0200

    Router and blank pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Language from './pages/Language';
+import Languages from './pages/Languages';
+import Profile from './pages/Profile';
+import Login from './pages/Login';
+import Home from './pages/Home';
+
 function App() {
   return (
     <>
-      <h1>LinguaFlow</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/languages' element={<Languages />} />
+          <Route path='/language/:id' element={<Language />} />
+        </Routes>
+      </BrowserRouter>{' '}
     </>
   );
 }
